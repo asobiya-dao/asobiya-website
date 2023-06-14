f630467aef7bfdfb2b058a35e92a234ccdf5911e
shokaji <shokaji.mail@gmail.com>
2023-06-15 00:51:16 +0900

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react';
 
 export const Hero = ({ title }: { title: string }) => (
   <Flex
@@ -8,10 +8,10 @@ export const Hero = ({ title }: { title: string }) => (
     bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
     bgClip="text"
   >
-    <Heading fontSize="6vw">{title}</Heading>
+    <Heading fontSize="10vw" color="black">{title}</Heading>
   </Flex>
-)
+);
 
 Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
-}
+  title: 'asobiya',
+};
