276141a9393baad6fde756b84f67305d8f039e3f
shokaji <shokaji.mail@gmail.com>
2023-07-08 11:04:25 +0900

    使用してないボタンの削除

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,26 +29,8 @@ export default function WithBackgroundImage() {
             lineHeight={1.2}
             fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
           >
-            テスト
+            ここに何かテキストとボタン追加するかも？
           </Text>
-          <Stack direction={'row'}>
-            <Button
-              bg={'blue.400'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'blue.500' }}
-            >
-              Show me more
-            </Button>
-            <Button
-              bg={'whiteAlpha.300'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'whiteAlpha.500' }}
-            >
-              Show me more
-            </Button>
-          </Stack>
         </Stack>
       </VStack>
     </Flex>
