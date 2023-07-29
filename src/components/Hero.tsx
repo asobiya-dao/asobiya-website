49f849f64333c2b8b63c764bd56c7df3c6bcda2b
shokaji <shokaji.mail@gmail.com>
2023-07-29 09:02:36 +0900

    ・Heroの画像をsvgファイルに変更しました。 ・Hero.tsxを修正しました。

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,28 +4,11 @@ export default function WithBackgroundImage() {
   return (
     <Flex
       w={'full'}
-      h={'100vh'}
-      backgroundImage={'/images/header.png'}
-      backgroundSize={'cover'}
+      h={'80vh'}
+      backgroundImage={'/images/hero.svg'}
+      backgroundSize={'contain'}
       backgroundPosition={'center center'}
-    >
-      <VStack
-        w={'full'}
-        justify={'center'}
-        px={useBreakpointValue({ base: 4, md: 8 })}
-        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
-      >
-        <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
-          <Text
-            color={'white'}
-            fontWeight={700}
-            lineHeight={1.2}
-            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
-          >
-            ここに何かテキストとボタン追加するかも？
-          </Text>
-        </Stack>
-      </VStack>
-    </Flex>
+      backgroundRepeat={'no-repeat'}
+    ></Flex>
   )
 }
